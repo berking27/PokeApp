0dd01ff0105c789b1c24bb60804e4d3a7130c47b
Berkin Demirel <berkin.koca01@gmail.com>
2024-12-04 00:09:05 +0300

    Widgets added.

--- PokeApp/CoreData/Persistence.swift
+++ PokeApp/CoreData/Persistence.swift
@@ -41,9 +41,13 @@ struct PersistenceController {
 
     init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "PokeApp")
+        
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        } else {
+            container.persistentStoreDescriptions.first!.url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.com.berkindemirel.PokeAppGroup")!.appending(path: "PokeApp.sqlite")
         }
+        
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
 
